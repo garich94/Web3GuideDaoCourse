f3a2d6f3db93845de350dd92f5fcc52fd84d692e
garich94 <garkavenkoaa@yandex.ru>
2023-05-25 18:33:11 +0300

    hi

---
+++ contexts/AppContext.js
@@ -0,0 +1,22 @@
+import React, { createContext, useState, useEffect } from "react";
+
+export const AppContext = createContext();
+
+export const AppContextProvider = ({ children }) => {
+  const [contextState, setContextState] = useState();
+
+  const updateContextState = (newContext) => {
+    setContextState((prevContext) => ({ ...prevContext, ...newContext }));
+  };
+
+  return (
+    <AppContext.Provider
+      value={{
+        contextState,
+        updateContextState,
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
+};
---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  env: {
+    networkName: "goerli",
+  },
+};
+
+module.exports = nextConfig;
